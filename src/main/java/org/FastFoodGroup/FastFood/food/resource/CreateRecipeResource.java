605080514bff8e34865d540d98657b86efb8d746
Charlie <u202120841@upc.edu.pe>
2023-11-05 10:42:40 -0500

    feat: implement resource

--- src/main/java/org/FastFoodGroup/FastFood/food/resource/CreateRecipeResource.java
+++ src/main/java/org/FastFoodGroup/FastFood/food/resource/CreateRecipeResource.java
@@ -1,4 +1,30 @@
 package org.FastFoodGroup.FastFood.food.resource;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Getter
+@Setter
+@With
+@AllArgsConstructor
+@NoArgsConstructor
 public class CreateRecipeResource {
+    @NotNull
+    private Integer recipeId;
+    @NotNull
+    private String name;
+    // photo
+    @NotNull
+    private String photo;
+    // duration
+    @NotNull
+    private Integer duration;
+    // stars
+    @NotNull
+    private Integer stars;
+    @NotNull
+    private String description;
+    @NotNull
+    private String preparation;
+
 }
